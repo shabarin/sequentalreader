a8d39d3e7b10e31533b0c25993d8ee1b574e11c7
shabarin <ivan@shabarin.ru>
2016-08-30 19:40:58 +0300

    tests

--- test/test.js
+++ test/test.js
@@ -7,13 +7,15 @@ let getStreamSequentalReader = require('../index');
 /**
  * creates ReadableStream that generates stream of 0, 1, 2 ... 9, 0, 1, 2, ...
  * @options.maxSize the maximum count of elements until eof
+ * @options.errorAfter the number of elements after which the stream emits error
  */
-class MyReadable extends stream.Readable {
+class MyTestReadable extends stream.Readable {
 
     constructor(options) {
         super(options);
         this._num = 0;
         this._maxSize = options.maxSize;
+        this._errorAfter = options.errorAfter || null;
     }
 
     _read() {
@@ -25,6 +27,9 @@ class MyReadable extends stream.Readable {
                 let data = new Buffer ( (this._num % 10).toString(), 'utf8');
                 let res = this.push(data.toString());
                 this._num++;
+                if (this._errorAfter !== null && this._num > this._errorAfter) {
+                    this.emit('error', new Error('test error'));
+                }
                 if (res === false) break;
             }
         }
@@ -32,7 +37,7 @@ class MyReadable extends stream.Readable {
 }
 
 /**
- * Checks that data buffer contains the same data as it would have been emitted by MyReadable stream
+ * Checks that data buffer contains the same data as it would have been emitted by MyTestReadable stream
  * @param data
  * @param length
  * @param initialNum
@@ -51,7 +56,7 @@ function checkMyReadable(data, length, initialNum) {
 describe('sequentalreader', function () {
 
     it('Reads correct data from stream', function (done) {
-        let testStream = new MyReadable({ maxSize: 1000 });
+        let testStream = new MyTestReadable({ maxSize: 1000 });
         let readNext = getStreamSequentalReader(testStream);
         readNext(1000)
             .then((res) => {
@@ -61,7 +66,7 @@ describe('sequentalreader', function () {
     });
 
     it('Handles end of stream', function(done) {
-        let testStream = new MyReadable({ maxSize: 1000 });
+        let testStream = new MyTestReadable({ maxSize: 1000 });
         let readNext = getStreamSequentalReader(testStream);
         readNext(1001)
             .then((res) => {
@@ -71,7 +76,7 @@ describe('sequentalreader', function () {
     });
 
     it('Correctly handles multiple sequental reads', function(done) {
-        let testStream = new MyReadable({ maxSize: 100 });
+        let testStream = new MyTestReadable({ maxSize: 100 });
         let readNext = getStreamSequentalReader(testStream);
         let buf = new Buffer(0);
         readNext(10)
@@ -91,7 +96,7 @@ describe('sequentalreader', function () {
     });
 
     it('Correctly handles multiple sequental reads and eof', function(done) {
-        let testStream = new MyReadable({ maxSize: 99 });
+        let testStream = new MyTestReadable({ maxSize: 99 });
         let readNext = getStreamSequentalReader(testStream);
         let buf = new Buffer(0);
         readNext(10)
@@ -111,7 +116,7 @@ describe('sequentalreader', function () {
     });
 
     it('Correctly handles multiple sequental reads and eof on chunk boundary', function(done) {
-        let testStream = new MyReadable({ maxSize: 30 });
+        let testStream = new MyTestReadable({ maxSize: 30 });
         let readNext = getStreamSequentalReader(testStream);
         let buf = new Buffer(0);
         readNext(10)
@@ -131,12 +136,40 @@ describe('sequentalreader', function () {
     });
 
     it('Correctly handles empty stream', function(done) {
-        let testStream = new MyReadable({ maxSize: 0 });
+        let testStream = new MyTestReadable({ maxSize: 0 });
         let readNext = getStreamSequentalReader(testStream);
         readNext(10)
             .then((res) => {
                 if (res === null) done();
                 else done('error')
+            })
+    });
+
+    it('Correctly handles stream exceptions', function(done) {
+        let testStream = new MyTestReadable({ maxSize: 1000, errorAfter: 5 });
+        let readNext = getStreamSequentalReader(testStream);
+        readNext(10)
+            .then((res) => {
+                done('error not handled properly')
+            })
+            .catch((e) => {
+                done();
+            })
+    });
+
+    it('Correctly handles stream exceptions after multiple reads', function(done) {
+        let testStream = new MyTestReadable({ maxSize: 1000, errorAfter: 50 });
+        let readNext = getStreamSequentalReader(testStream);
+        readNext(45)
+            .then((res) => {
+                if (res.length != 45) done('first read failed');
+                return readNext(10)
+            })
+            .then((res) => {
+                done('error not handled properly')
+            })
+            .catch((e) => {
+                done();
             })
     });
 });
